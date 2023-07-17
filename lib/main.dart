473b144926fd0bbcd8e3ec74c60f98ee4ed62e25
Satyajit033 <satyajitbiswas033@gmail.com>
2023-07-17 23:44:06 +0600

    this is module 10 live test

---
+++ lib/main.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'News Feed',
+      theme: ThemeData(primaryColor: Colors.teal),
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Feed'),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.landscape) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 500,
+                    height: 500,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+
+                      children: [
+                        Expanded(
+                            child: GridView.count(
+                                crossAxisSpacing: 7,
+                                mainAxisSpacing: 7,
+                                crossAxisCount: 2,
+                                // Number of columns
+                                children: [
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+                                  Container(
+                                    child:Center(
+                                      child: Container(
+                                        height: 180,
+                                        width: 180,
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Text(
+                                            '150*150',
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                        color: Colors.white54,
+                                      ),
+                                    ),
+                                    color: Colors.blueGrey,
+                                    height: 120,
+                                    width: 120,
+                                  ),
+
+
+                                ])),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            );
+          } else {
+            // Return a different widget when the orientation is not landscape
+            return Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: GridView.count(
+                            crossAxisSpacing: 7,
+                            mainAxisSpacing: 7,
+                            crossAxisCount: 1,
+                            // Number of columns
+                            children: [
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+                              Container(
+                                child:Center(
+                                  child: Container(
+                                    height: 350,
+                                    width: 350,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '150*150',
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    color: Colors.white54,
+                                  ),
+                                ),
+                                color: Colors.blueGrey,
+                                height: 120,
+                                width: 120,
+                              ),
+
+
+                            ])),
+                  ]),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
